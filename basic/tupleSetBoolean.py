29882344323735d39c6f498c5d5613819a14bf24
Ram Sthapit <ramsthapit50@gmail.com>
2021-12-25 20:11:11 +0545

    tuples, booleans & sets

---
+++ basic/tupleSetBoolean.py
@@ -0,0 +1,29 @@
+#Booleans
+True
+False
+0
+1
+
+#Tuples
+t =(1,2,3)
+print(t[0]) 
+
+t = ('a', True, 123)
+print(t)
+
+#Lists
+t = ['a', True, 123]
+t[0] = 'New'
+print(t)
+
+#sets
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+print(x)
+
+y = set([1,2,2,2,4,5,5])
+print(y)
