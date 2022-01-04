5523203bcd014a73b70ad75205de73dcdb66009f
Ram Sthapit <ramsthapit50@gmail.com>
2022-01-04 16:37:51 +0545

    templates

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict = {'insert_me': "Hello I am from Views.py"}
+    return render(request, 'first_app\index.html', context=my_dict)
