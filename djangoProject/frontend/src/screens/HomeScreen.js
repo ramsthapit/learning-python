822c8153b456f3d23bb6f0fc1c768f7d51ad1719
Ram Sthapit <ramsthapit50@gmail.com>
2022-01-19 15:12:41 +0545

    implementing redux

--- djangoProject/frontend/src/screens/HomeScreen.js
+++ djangoProject/frontend/src/screens/HomeScreen.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProducts } from '../actions/productActions'
+import { Row } from 'react-bootstrap'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state=>state.productList)
+  const { loading, error, products } = productList
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+  console.log(products);
   return (
-    <div>
-      Home Screen
-    </div>
+    <>
+      <h1>Latest Products</h1>
+      {loading ? <Loader /> : error ? <Message variant='danger' >{error} </Message> :
+        <Row>
+          {/* {products.map(product => (
+            console.log(product.name)
+          ))} */}
+        </Row>
+      }
+    </>
   )
 }
 
