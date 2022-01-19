b0795e43edf3d699ce6f440363037d13775bb706
Ram Sthapit <ramsthapit50@gmail.com>
2022-01-19 20:46:37 +0545

    data entry

--- djangoProject/backend/base/urls/product_urls.py
+++ djangoProject/backend/base/urls/product_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from base.views import product_views as views
 
 
 urlpatterns = [
--- djangoProject/backend/base/urls/user_urls.py
+++ djangoProject/backend/base/urls/user_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from base.views import user_views as views
 
 
 urlpatterns = [
