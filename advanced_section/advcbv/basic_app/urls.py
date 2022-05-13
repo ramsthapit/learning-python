6d3c31002d10037be8442d76d988b28f8a137032
Ram Sthapit <ramsthapit50@gmail.com>
2022-05-13 22:30:12 +0545

    CRUD views

--- advanced_section/advcbv/basic_app/urls.py
+++ advanced_section/advcbv/basic_app/urls.py
@@ -5,6 +5,10 @@ app_name = 'basic_app'
 
 urlpatterns = [
     path('', views.SchoolListView.as_view(), name='list'),
-    path('<str:pk>/', views.SchoolDetailView.as_view(), name="detial"),
+    path('create/', views.SchoolCreateView.as_view(), name='create'),
+    path('update/<str:pk>/', views.SchoolUpdateView.as_view(), name="update"),
+    path('delete/<str:pk>/', views.SchoolDeleteView.as_view(), name="delete"),
+    path('<str:pk>/', views.SchoolDetailView.as_view(), name="detail"),
 
 ]
+ 
